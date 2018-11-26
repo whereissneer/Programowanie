075a99ae516a824ba9c53cf3c3a812c74e901332
Robert <robertm1013@gmail.com>
2018-11-26 21:54:30 +0100

    Dodanie main.cpp za listopad

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+void zadanie1_1() {
+	int a = 1230;
+	float pi = 3.14159265;
+	char imie[6] = { 'R','O','B','E','R','T' };
+	std::cout << a << endl << pi << endl << imie << endl;
+	}
+void zadanie1_2() {
+	int b = 2018;
+	printf("%d", b);
+	}
+void zadanie1_3() {
+	int x;
+
+	}
+int main() {
+		zadanie1_1();
+		std::system("Pause");
+		return 0;
+	}
